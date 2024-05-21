5928d36dbfede6d68a76353cfd2da0a948e26d0e
cloth <pbp0417@gmail.com>
2024-05-21 10:10:54 +0900

    현재까지 수업했던 html, javascript 예제 추가

---
+++ javascript/test02.js
@@ -0,0 +1,16 @@
+// test02.js
+/*
+    html요소에 일어나는 이벤트 종류
+
+    클릭하면 => click
+    마우스 커서를 올리면 => mouseover
+    마우스 커서가 요소를 벗어나면 => mouseout
+    마우스를 클릭하면 => mousedown
+    마우스를 클릭 후 떼면 => mouseup
+    포커스가 오면 => focuse
+    포커스를 잃으면 => blur     ...등등
+*/
+
+document.querySelector("#one").innerText = "p1 입니다";
+
+orac
